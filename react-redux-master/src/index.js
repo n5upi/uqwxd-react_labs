cb7e05319157067b7026ed9873816c1f68e31c4c
n5upi <dallasats@hotmail.com>
2024-05-15 04:13:54 -0400

    Adding temporary changes to Github

--- react-redux-master/src/index.js
+++ react-redux-master/src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
-import {createStore} from 'redux';
+//import {createStore} from 'redux';
 import {Provider} from 'react-redux'
 import myReducers from './reducers'
+import {legacy_createStore as createStore} from 'redux';
+
+
 
 
 //Create the store
@@ -22,3 +25,11 @@ ReactDOM.render(<Provider store={myStore}><App/></Provider>, document.getElement
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
+const increment = (val) => {
+    return {
+        type : 'INCREMENT',
+        inc : val
+    }
+}
+
+export default increment;
